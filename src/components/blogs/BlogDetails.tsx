577017131afb87f1773637773e303c8d1c112ca3
Asif <asifabdullah135@gmail.com>
2024-01-08 10:56:14 +0600

    edit blog modal added

--- src/components/blogs/BlogDetails.tsx
+++ src/components/blogs/BlogDetails.tsx
@@ -1,25 +1,35 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import {jwtDecode} from 'jwt-decode'
 import AuthService from "../../services/auth/authService";
 import Delete from "../common/Delete";
+import EditBlogModal from "./EditBlogModal";
 export default function BlogDetails() {
-    const [currentUser, setCurrentUser] = useState<{accessToken: string} | null>(null);
+    const [currentUserNameName, setCurrentUser] = useState<string | null>(null);
     const [confirmationOpen, setConfirmationOpen] = useState(false);
-    const location = useLocation();
-    const blog = location.state;
+    const [showModal, setShowModal] = useState(false);
+    const [showEditAlertMessage, setEditAlertMessage] = useState(false);
 
+    const location = useLocation();
+    const [blog, setBlog] = useState(location.state);
 
     useEffect(() => {
-        const user = AuthService.getCurrentUser();
+        let timer: ReturnType<typeof setTimeout>;
+
+        if (showEditAlertMessage) {
+            timer = setTimeout(() => {
+                setEditAlertMessage(false);
+            }, 3000);
+        }
+        return () => clearTimeout(timer);
+    }, [showEditAlertMessage]);
+
+    useEffect(() => {
+        const user = AuthService.getCurrentUserName();
         setCurrentUser(user);
     }, []);
     const canEditOrDelete = () => {
-        if (currentUser && currentUser.accessToken) {
-            const decodedToken = jwtDecode<MyJwtPayload>(currentUser.accessToken);
-            return decodedToken.username === blog.username;
-        }
-        return false;
+        return currentUserNameName === blog.username;
     }
     const handleDelete = async () => {
         if (canEditOrDelete()) {
@@ -29,10 +39,18 @@ export default function BlogDetails() {
 
     const handleEdit = () => {
         if (canEditOrDelete()) {
-
+            setShowModal(true);
         }
     };
+
+    const updateBlogData = (updatedBlog: any) => {
+        setBlog(updatedBlog);
+        setEditAlertMessage(true);
+    };
+
+
     return (
+        <>
         <div className="max-w-3xl mx-auto mt-8 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div className="mb-4">
                 <h1 className="text-3xl text-gray-100 font-semibold mb-2">{blog.title}</h1>
@@ -50,6 +68,7 @@ export default function BlogDetails() {
                         >
                             Edit
                         </button>
+
                         <button
                             onClick={handleDelete}
                             className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -66,11 +85,27 @@ export default function BlogDetails() {
                     setOpen={setConfirmationOpen}
                 />
             )}
-
+            {showModal && (
+                <EditBlogModal
+                    blog={blog}
+                    setShowModal={setShowModal}
+                    updateBlogData={updateBlogData}
+                />
+            )}
         </div>
+            {showEditAlertMessage &&
+                <div className="fixed bottom-10 right-6">
+                <div className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+                <div className="flex">
+                    <div className="py-1"><svg className="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+                    <div>
+                        <p className="font-bold">Blog edited successfully</p>
+                    </div>
+                </div>
+            </div>
+                </div>}
+        </>
+
     );
 }
 
-interface MyJwtPayload {
-    username: string;
-}

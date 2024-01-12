3409463a920037c70a48bbfc1aabd865f01d1890
Asif <asifabdullah135@gmail.com>
2024-01-12 17:57:41 +0600

    not found page added

---
+++ src/components/common/PageNotFound.tsx
@@ -0,0 +1,14 @@
+const PageNotFound = () => {
+    return (
+        <>
+            <div>
+                <h1
+                className="text-5xl p-4 text-center font-bold from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent">
+                    404 Not Found
+                </h1>
+            </div>
+        </>
+    );
+}
+
+export default PageNotFound
